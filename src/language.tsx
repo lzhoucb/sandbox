8eb0248ae404dac480a1e64071c3dc7e29888a63
Langchun Zhou <lzhou@collegeboard.org>
2025-05-30 14:44:11 -0400

    Added RadioGroup

--- src/language.tsx
+++ src/language.tsx
@@ -7,16 +7,38 @@ export default {
 
 export const LanguageFonts = () => {
   const [lang, setLang] = useState("");
+  const [font, setFont] = useState("Times");
 
   return (
     <div className="lang-container">
-      <div className="lang-radio">
+      <div className="radio-group">
         <p>lang</p>
-        <LangRadio lang="" langName="None (empty string)" curLang={lang} setLang={setLang} />
+        {/* <Radio value="" label="None (empty string)" name="lang" id="lang-none" curValue={lang} setValue={setLang} />
         <br />
-        <LangRadio lang="zh" curLang={lang} setLang={setLang} />
+        <Radio value="zh" label="zh" name="lang" id="lang-zh" curValue={lang} setValue={setLang} /> */}
+        <RadioGroup
+          name="lang"
+          curValue={lang}
+          setValue={setLang}
+          table={[
+            {value: "", label: "None (empty string)", idSuffix: "none"},
+            {value: "zh", label: "zh", idSuffix: "zh"}
+          ]}
+        />
       </div>
-      <div id="chinese" lang={lang}>
+      <div className="radio-group">
+        <p>font</p>
+        <Radio value="Times" label="Times" name="font" id="font-times" curValue={font} setValue={setFont}/>
+        <br/>
+        <Radio value="Songti SC" label="Songti SC" name="font" id="font-songti-sc" curValue={font} setValue={setFont}/>
+        <br/>
+        <Radio value="Songti TC" label="Songti TC" name="font" id="font-songti-tc" curValue={font} setValue={setFont}/>
+        <br/>
+        <Radio value="PingFang SC" label="PingFang SC" name="font" id="font-pingfang-sc" curValue={font} setValue={setFont}/>
+        <br/>
+        <Radio value="PingFang TC" label="PingFang TC" name="font" id="font-pingfang-tc" curValue={font} setValue={setFont}/>
+      </div>
+      <div id="chinese" lang={lang} style={{fontFamily: font}}>
         <div id="trad-chinese">
           <p>Traditional</p>
           <p>
@@ -48,31 +70,52 @@ export const LanguageFonts = () => {
   );
 };
 
-interface LangRadioProps {
-  lang: string;
-  langName?: string;
-  curLang: string;
-  setLang: (lang: string) => void;
+interface RadioProps {
+  value: string;
+  label: string;
+  name: string;
+  id: string;
+  curValue: string;
+  setValue: (value: string) => void;
 }
 
-const LangRadio: React.FC<LangRadioProps> = ({ lang, langName, curLang, setLang }) => {
-  const displayName = langName ?? lang;
-
+const Radio: React.FC<RadioProps> = ({ value, label, name, id, curValue, setValue }) => {
   return (
     <>
       <input
         type="radio"
-        checked={curLang === lang}
-        value={displayName}
-        name="lang"
-        id={displayName}
+        checked={curValue === value}
+        value={value}
+        name={name}
+        id={id}
         onChange={event => {
-          if (event.target.value === displayName) {
-            setLang(lang);
+          if (event.target.value === value) {
+            setValue(value);
           }
         }}
       />
-      <label htmlFor={displayName}>{displayName}</label>
+      <label htmlFor={id}>{label}</label>
     </>
   );
 };
+
+interface RadioGroupProps{
+  name: string;
+  curValue: string;
+  setValue: (value: string)=>void;
+  table: Array<{value: string, label: string, idSuffix: string}>;
+}
+
+const RadioGroup: React.FC<RadioGroupProps> = ({name, curValue, setValue, table}) => {
+  const radios = [];
+
+  for(const {value, label, idSuffix} of table){
+    const id = `${name}-${idSuffix}`;
+    radios.push(<>
+      <Radio key={id} value={value} label={label} name={name} id={id} curValue={curValue} setValue={setValue}/>
+      <br/>
+    </>);
+  }
+
+  return radios;
+}
